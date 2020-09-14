157aea1ff9226bd3056cd36e912d52ab3d966240
Lubos Hasko <lubos@luboshasko.com>
2020-09-14 13:38:39 +1000

    no message

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Localizations
+{
+    class Program
+    {
+        static void Main()
+        {
+            var projectDir = new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.Parent;
+
+            var output = new Dictionary<string, Dictionary<string, object>>();
+
+            foreach (var e in projectDir.GetDirectories().OrderBy(x => x.Name))
+            {
+                if (e.Name.StartsWith('.')) continue;
+                if (e.Name == "obj") continue;
+                if (e.Name == "bin") continue;
+
+                output.Add(e.Name, new Dictionary<string, object>());
+
+                foreach (var e2 in e.GetFiles("*.json").OrderBy(x => x.Name))
+                {
+                    Console.WriteLine("Input: "+e2.FullName);
+
+                    var json = JsonConvert.DeserializeObject(File.ReadAllText(e2.FullName));
+
+                    var name = Path.GetFileNameWithoutExtension(e2.Name);
+                    output[e.Name].Add(name, json);
+                }
+            }
+
+            var outputJson = JsonConvert.SerializeObject(output, Formatting.Indented);
+            var outputPath = Path.Combine(Directory.GetCurrentDirectory(), "Localizations.json");
+            Console.WriteLine("Output: " + outputPath);
+            File.WriteAllText(outputPath, outputJson);
+        }
+    }
+}
